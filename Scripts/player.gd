698790db959929c7532af177f1ce3a0adbc5a840
Theo Fernandes dos Santos Dias <l0n3l.dev@gmail.com>
2025-07-13 18:15:33 -0300

    Polishing movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,21 +1,29 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var acceleration = 800.0
+@export var friction = 1000.0
 
 @export var jumpAmount:int = 2
 var jumps = jumpAmount
+
+@export var fastFallMultiplier = 2.0
 
 func _process(delta: float) -> void:
 	Animate()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	var fastFall = 1
+	if velocity.y > 0:
+		fastFall = fastFallMultiplier
+	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * fastFall * delta
 	else:
-		velocity = Vector2(0, 0)
+		velocity.y = 0
 		jumps = jumpAmount
 
 	# Handle jump.
@@ -28,10 +36,11 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Move Left", "Move Right")
+	var targetSpeed = direction * SPEED
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, targetSpeed, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
 
 	move_and_slide()
 
